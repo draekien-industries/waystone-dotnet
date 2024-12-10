6bf7379787db533587b76ef47a26abcb5a5045b5
William Pei <draekien@users.noreply.github.com>
2024-12-10 21:57:19 +1100

    test: option tests

---
+++ test/Waystone.Monads.Tests/NoneTest.cs
@@ -0,0 +1,121 @@
+﻿namespace Waystone.Monads.Tests;
+
+using Exceptions;
+
+[TestSubject(typeof(None<>))]
+public class NoneTest
+{
+    [Fact]
+    public void GivenNone_WhenAccessingValue_ThenThrow()
+    {
+        IOption<int> none = Option.None<int>();
+
+        none.IsSome.Should().BeFalse();
+        none.IsNone.Should().BeTrue();
+
+        none.IsSomeAnd(_ => true).Should().BeFalse();
+        none.IsNoneOr(_ => false).Should().BeTrue();
+
+        none.Invoking(x => x.Unwrap()).Should().Throw<UnwrapException>();
+        none.Invoking(x => x.Expect("value is 1"))
+            .Should()
+            .Throw<UnmetExpectationException>()
+            .WithMessage("Value is 1");
+    }
+
+    [Fact]
+    public void GivenNone_WhenAccessingValueWithFallback_ThenReturnFallback()
+    {
+        IOption<int> none = Option.None<int>();
+
+        none.UnwrapOr(10).Should().Be(10);
+        none.UnwrapOrDefault().Should().Be(default);
+        none.UnwrapOrElse(() => 10).Should().Be(10);
+    }
+
+    [Fact]
+    public void GivenNone_WhenMatchWithActions_ThenInvokeOnNone()
+    {
+        IOption<int> none = Option.None<int>();
+
+        var onSome = Substitute.For<Action<int>>();
+        var onNone = Substitute.For<Action>();
+
+        none.Match(onSome, onNone);
+
+        onSome.DidNotReceive().Invoke(Arg.Any<int>());
+        onNone.Received(1).Invoke();
+    }
+
+    [Fact]
+    public void GivenNone_WhenMatchWithFunc_ThenInvokeOnNone()
+    {
+        IOption<int> none = Option.None<int>();
+
+        var onSome = Substitute.For<Func<int, int>>();
+        onSome.Invoke(Arg.Any<int>()).Returns(1);
+
+        var onNone = Substitute.For<Func<int>>();
+        onNone.Invoke().Returns(10);
+
+        int output = none.Match(onSome, onNone);
+
+        output.Should().Be(10);
+    }
+
+    [Fact]
+    public void GivenNone_WhenMapWithFallback_ThenReturnDefault()
+    {
+        IOption<int> none = Option.None<int>();
+
+        none.MapOr(10, x => x + 1).Should().Be(10);
+        none.MapOrElse(() => 10, x => x + 1).Should().Be(10);
+    }
+
+    [Fact]
+    public void GivenNone_WhenInspect_ThenDoNothing()
+    {
+        IOption<int> none = Option.None<int>();
+
+        var action = Substitute.For<Action<int>>();
+
+        IOption<int> result = none.Inspect(action);
+
+        result.Should().Be(none);
+        action.DidNotReceive().Invoke(Arg.Any<int>());
+    }
+
+    [Fact]
+    public void GivenNone_WhenFilter_ThenDoNothing()
+    {
+        IOption<int> none = Option.None<int>();
+
+        var filter = Substitute.For<Predicate<int>>();
+
+        IOption<int> result = none.Filter(filter);
+
+        result.Should().Be(none);
+        filter.DidNotReceive().Invoke(Arg.Any<int>());
+    }
+
+    [Fact]
+    public void GivenNone_WhenOr_ThenReturnOther()
+    {
+        IOption<int> none = Option.None<int>();
+
+        none.Or(Option.None<int>()).Should().Be(none);
+        none.Or(Option.Some(1)).Should().Be(Option.Some(1));
+
+        none.OrElse(Option.None<int>).Should().Be(Option.None<int>());
+        none.OrElse(() => Option.Some(1)).Should().Be(Option.Some(1));
+    }
+
+    [Fact]
+    public void GivenNone_WhenXor_ThenReturnExpected()
+    {
+        IOption<int> none = Option.None<int>();
+
+        none.Xor(Option.None<int>()).Should().Be(Option.None<int>());
+        none.Xor(Option.Some(1)).Should().Be(Option.Some(1));
+    }
+}
