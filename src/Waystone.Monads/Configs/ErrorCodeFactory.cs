29d4757aa64dbc0c65bad4189c326edc6dc61a3f
draekien <draekien@users.noreply.github.com>
2025-07-02 16:03:31 +1000

    fix: debug write line is stripped from nuget package

--- src/Waystone.Monads/Configs/ErrorCodeFactory.cs
+++ src/Waystone.Monads/Configs/ErrorCodeFactory.cs
@@ -10,22 +10,22 @@ public class ErrorCodeFactory
 {
     private const string NameOfException = nameof(Exception);
 
-/// <summary>
-/// Creates a new instance of <see cref="ErrorCode"/> from an Enum value.
-/// </summary>
-/// <param name="enum">The enum value to convert into an Error Code.</param>
-/// <returns>The created <see cref="ErrorCode"/>.</returns>
+    /// <summary>
+    /// Creates a new instance of <see cref="ErrorCode"/> from an Enum value.
+    /// </summary>
+    /// <param name="enum">The enum value to convert into an Error Code.</param>
+    /// <returns>The created <see cref="ErrorCode"/>.</returns>
     public virtual ErrorCode FromEnum(Enum @enum)
     {
         var enumType = @enum.GetType();
         return new ErrorCode($"{enumType.Name}.{@enum}");
     }
 
-/// <summary>
-/// Creates a new instance of <see cref="ErrorCode"/> from an Exception value.
-/// </summary>
-/// <param name="exception">The exception value to convert into an Error Code.</param>
-/// <returns>The created <see cref="ErrorCode"/>.</returns>
+    /// <summary>
+    /// Creates a new instance of <see cref="ErrorCode"/> from an Exception value.
+    /// </summary>
+    /// <param name="exception">The exception value to convert into an Error Code.</param>
+    /// <returns>The created <see cref="ErrorCode"/>.</returns>
     public virtual ErrorCode FromException(Exception exception)
     {
         var exceptionType = exception.GetType();
--- src/Waystone.Monads/Configs/MonadOptions.cs
+++ src/Waystone.Monads/Configs/MonadOptions.cs
@@ -24,8 +24,20 @@ public class MonadOptions
     internal Option<Action<Exception, CallerInfo>> ExceptionLogger { get; set; }
     internal ErrorCodeFactory ErrorCodeFactory { get; set; }
 
-    internal void Log(Exception exception, CallerInfo callerInfo) =>
+    internal void Log(Exception exception, CallerInfo callerInfo)
+    {
+        if (Debugger.IsAttached)
+        {
+            Console.WriteLine("[Waystone.Monads] Exception silently handled:");
+            Console.WriteLine($"  Message: {exception.Message}");
+            Console.WriteLine($"  Type: {exception.GetType().FullName}");
+            Console.WriteLine($"  StackTrace: {exception.StackTrace}");
+            Console.WriteLine($"  Caller: {callerInfo.MemberName} at line {callerInfo.LineNumber}");
+            Console.WriteLine($"  Argument Expression: {callerInfo.ArgumentExpression}");
+        }
+
         ExceptionLogger.Inspect(logger => logger.Invoke(exception, callerInfo));
+    }
 
     /// <summary>
     /// Configures the log action that should be executed when an exception is silently
