53c36db64550cfb35ba05760d6120937e5fcbc68
William Pei <draekien@users.noreply.github.com>
2025-08-02 11:50:42 +1000

    test: iter compare

---
+++ test/Waystone.Monads.Tests/Iterators/Steps/CompareSteps.cs
@@ -0,0 +1,34 @@
+﻿namespace Waystone.Monads.Iterators.Steps;
+
+using System;
+using System.Collections.Generic;
+using Extensions;
+using Reqnroll;
+using Shouldly;
+
+[Binding]
+public class CompareSteps(ScenarioContext context)
+{
+    [When("comparing {string} and {string} of integers")]
+    public void WhenComparingAndOfIntegers(string leftKey, string rightKey)
+    {
+        Iter<int> left = context.Get<IEnumerable<int>>(leftKey).IntoIter();
+        Iter<int> right = context.Get<IEnumerable<int>>(rightKey).IntoIter();
+
+        Ordering result = left.Compare(right);
+
+        context.Set(result);
+    }
+
+    [Then("the Ordering should be {string}")]
+    public void ThenTheOrderingShouldBe(string equal)
+    {
+        var result = context.Get<Ordering>();
+        if (!Enum.TryParse(equal, out Ordering expected))
+        {
+            throw new ArgumentException($"Unknown ordering: {equal}");
+        }
+
+        result.ShouldBe(expected);
+    }
+}
---
+++ test/Waystone.Monads.Tests/Iterators/Steps/IterSteps.cs
@@ -0,0 +1,49 @@
+﻿namespace Waystone.Monads.Iterators.Steps;
+
+using System.Collections.Generic;
+using System.Linq;
+using Extensions;
+using Reqnroll;
+using Shouldly;
+
+[Binding]
+public sealed class IterSteps(ScenarioContext context)
+{
+    [Given("an {string} of integers from {int} to {int}")]
+    public void GivenAnEnumerableOfIntegersFromTo(
+        string key,
+        int start,
+        int end)
+    {
+        IEnumerable<int> enumerable = Enumerable.Range(start, end - start + 1);
+        context.Set(enumerable, key);
+    }
+
+    [When("converting {string} of integers to an iterator")]
+    public void WhenIConvertItToAnIterator(string key)
+    {
+        var enumerable = context.Get<IEnumerable<int>>(key);
+        Iter<int> iter = enumerable.IntoIter();
+        context.Set(iter, key);
+    }
+
+    [Then("the {string} iterator of integers should yield")]
+    public void ThenTheIteratorOfIntegersShouldYield(
+        string enumerable,
+        Table table)
+    {
+        var iter = context.Get<Iter<int>>(enumerable);
+        List<int> expected =
+            table.Rows.Select(row => int.Parse(row["Value"])).ToList();
+        List<int> actual = iter.Elements.ToList();
+
+        actual.ShouldBe(expected);
+    }
+
+    [Given("an empty {string} of integers")]
+    public void GivenAnEmptyOfIntegers(string key)
+    {
+        IEnumerable<int> enumerable = [];
+        context.Set(enumerable, key);
+    }
+}
