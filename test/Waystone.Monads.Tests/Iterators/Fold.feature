e62e01e3bf558507c723c14dc5065dd787bf8e1d
William Pei <draekien@users.noreply.github.com>
2025-08-11 23:05:15 +1000

    test: fold

---
+++ test/Waystone.Monads.Tests/Iterators/Fold.feature
@@ -0,0 +1,7 @@
+﻿Feature: Iter.Fold
+
+    Scenario: Fold over an array
+        Given an "enumerable" of integers from 1 to 5
+        When converting "enumerable" of integers to an iterator
+        And folding the "enumerable" integer iterator with an add operation
+        Then the result of the folded "enumerable" should be 15
---
+++ test/Waystone.Monads.Tests/Iterators/Steps/FoldSteps.cs
@@ -0,0 +1,24 @@
+﻿namespace Waystone.Monads.Iterators.Steps;
+
+using Reqnroll;
+using Shouldly;
+
+[Binding]
+public class FoldSteps(ScenarioContext context)
+{
+    [When("folding the {string} integer iterator with an add operation")]
+    public void WhenFoldingTheIntegerIteratorWithAnAddOperation(
+        string enumerable)
+    {
+        var iter = context.Get<Iter<int>>(enumerable);
+        int result = iter.Fold(0, (acc, x) => acc + x);
+        context.Set(result, enumerable);
+    }
+
+    [Then("the result of the folded {string} should be {int}")]
+    public void ThenTheResultOfTheFoldedShouldBe(string enumerable, int p1)
+    {
+        var result = context.Get<int>(enumerable);
+        result.ShouldBe(p1);
+    }
+}
