ec23d0f5a0fdd8c8b708347755a518e5564ec485
William Pei <draekien@users.noreply.github.com>
2025-11-23 10:13:46 +1100

    test: add unit tests for IsSomeAnd and IsNoneOr methods

--- test/Waystone.Monads.Tests/Options/Extensions/AsyncControlFlowExtensionsTests.cs
+++ test/Waystone.Monads.Tests/Options/Extensions/AsyncControlFlowExtensionsTests.cs
@@ -98,4 +98,192 @@ public sealed class AsyncControlFlowExtensionsTests
 
         result.ShouldBeTrue();
     }
+
+    [Fact]
+    public async Task
+        GivenTaskSome_WhenIsSomeAndWithTruePredicate_ThenReturnTrue()
+    {
+        Task<Option<int>> some = Task.FromResult(Option.Some(10));
+
+        bool result = await some.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task
+        GivenTaskSome_WhenIsSomeAndWithFalsePredicate_ThenReturnFalse()
+    {
+        Task<Option<int>> some = Task.FromResult(Option.Some(10));
+
+        bool result = await some.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value < 5;
+        });
+
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task GivenTaskNone_WhenIsSomeAnd_ThenReturnFalse()
+    {
+        Task<Option<int>> none = Task.FromResult(Option.None<int>());
+
+        bool result = await none.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task
+        GivenTaskSome_WhenIsNoneOrWithTruePredicate_ThenReturnTrue()
+    {
+        Task<Option<int>> some = Task.FromResult(Option.Some(10));
+
+        bool result = await some.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task
+        GivenTaskNone_WhenIsNoneOrWithFalsePredicate_ThenReturnTrue()
+    {
+        Task<Option<int>> none = Task.FromResult(Option.None<int>());
+
+        bool result = await none.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value < 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task GivenTaskNone_WhenIsNoneOr_ThenReturnTrue()
+    {
+        Task<Option<int>> none = Task.FromResult(Option.None<int>());
+
+        bool result = await none.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task
+        GivenValueTaskSome_WhenIsSomeAndWithTruePredicate_ThenReturnTrue()
+    {
+        ValueTask<Option<int>> some = new(Option.Some(10));
+
+        bool result = await some.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task
+        GivenValueTaskSome_WhenIsSomeAndWithFalsePredicate_ThenReturnFalse()
+    {
+        ValueTask<Option<int>> some = new(Option.Some(10));
+
+        bool result = await some.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value < 5;
+        });
+
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task GivenValueTaskNone_WhenIsSomeAnd_ThenReturnFalse()
+    {
+        ValueTask<Option<int>> none = new(Option.None<int>());
+
+        bool result = await none.IsSomeAnd(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task
+        GivenValueTaskSome_WhenIsNoneOrWithTruePredicate_ThenReturnTrue()
+    {
+        ValueTask<Option<int>> some = new(Option.Some(10));
+
+        bool result = await some.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task
+        GivenValueTaskNone_WhenIsNoneOrWithFalsePredicate_ThenReturnTrue()
+    {
+        ValueTask<Option<int>> none = new(Option.None<int>());
+
+        bool result = await none.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value < 5;
+        });
+
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task GivenValueTaskNone_WhenIsNoneOr_ThenReturnTrue()
+    {
+        ValueTask<Option<int>> none = new(Option.None<int>());
+
+        bool result = await none.IsNoneOr(async value =>
+        {
+            await Task.Yield();
+
+            return value > 5;
+        });
+
+        result.ShouldBeTrue();
+    }
 }
