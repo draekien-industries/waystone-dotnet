8b67af89a29d7c935f4c2ad1fe09470cbfefc18e
William Pei <draekien@users.noreply.github.com>
2025-11-23 09:53:58 +1100

    feat: add asynchronous control flow methods for Option<T>

--- src/Waystone.Monads/Options/Extensions/AsyncControlFlowExtensions.cs
+++ src/Waystone.Monads/Options/Extensions/AsyncControlFlowExtensions.cs
@@ -10,10 +10,6 @@ using System.Threading.Tasks;
 /// </summary>
 public static class AsyncControlFlowExtensions
 {
-    /// <summary>
-    /// Provides extension methods for handling asynchronous control flow with
-    /// instances of <see cref="Option{T}" />.
-    /// </summary>
     extension<T>(Option<T> option) where T : notnull
     {
         /// <summary>
@@ -66,10 +62,6 @@ public static class AsyncControlFlowExtensions
         }
     }
 
-    /// <summary>
-    /// Provides extension methods for handling asynchronous control flow with
-    /// instances of <see cref="Option{T}" />.
-    /// </summary>
     extension<T>(Task<Option<T>> optionTask) where T : notnull
     {
         /// <summary>
@@ -81,13 +73,13 @@ public static class AsyncControlFlowExtensions
         /// <see cref="Option{T}" /> if it is in a "Some" state.
         /// </param>
         /// <returns>
-        /// A <see cref="ValueTask{TResult}" /> containing <see langword="true" /> if the
+        /// A <see cref="Task{TResult}" /> containing <see langword="true" /> if the
         /// <see cref="Option{T}" /> is in a "Some" state
         /// and the predicate evaluates to <see langword="true" /> for the contained value;
         /// otherwise, <see langword="false" />.
         /// </returns>
         [DebuggerStepThrough]
-        public async ValueTask<bool> IsSomeAnd(Func<T, Task<bool>> predicate)
+        public async Task<bool> IsSomeAnd(Func<T, Task<bool>> predicate)
         {
             Option<T> option = await optionTask.ConfigureAwait(false);
 
@@ -103,14 +95,63 @@ public static class AsyncControlFlowExtensions
         /// <see cref="Option{T}" /> if it is in a "Some" state.
         /// </param>
         /// <returns>
-        /// A <see cref="ValueTask{TResult}" /> containing <see langword="true" /> if the
+        /// A <see cref="Task{TResult}" /> containing <see langword="true" /> if the
         /// <see cref="Option{T}" /> is in a "None" state or the predicate evaluates to
         /// <see langword="true" /> for the contained value; otherwise,
         /// <see langword="false" />.
         /// </returns>
-        public async ValueTask<bool> IsNoneOr(Func<T, Task<bool>> predicate)
+        [DebuggerStepThrough]
+        public async Task<bool> IsNoneOr(Func<T, Task<bool>> predicate)
         {
             Option<T> option = await optionTask.ConfigureAwait(false);
+
+            return await option.IsNoneOr(predicate).ConfigureAwait(false);
+        }
+    }
+
+    extension<T>(ValueTask<Option<T>> optionValueTask) where T : notnull
+    {
+        /// <summary>
+        /// Asynchronously evaluates whether the <see cref="Option{T}" /> produced by the
+        /// ValueTask is in a "Some" state and satisfies the provided asynchronous
+        /// predicate.
+        /// </summary>
+        /// <param name="predicate">
+        /// An asynchronous function to test the value contained in the
+        /// <see cref="Option{T}" /> if it is in a "Some" state.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task{TResult}" /> containing <see langword="true" /> if the
+        /// <see cref="Option{T}" /> is in a "Some" state
+        /// and the predicate evaluates to <see langword="true" /> for the contained value;
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        [DebuggerStepThrough]
+        public async Task<bool> IsSomeAnd(Func<T, Task<bool>> predicate)
+        {
+            Option<T> option = await optionValueTask.ConfigureAwait(false);
+
+            return await option.IsSomeAnd(predicate).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Evaluates whether the current <see cref="Option{T}" /> instance is in a "None"
+        /// state or satisfies the provided asynchronous predicate when in a "Some" state.
+        /// </summary>
+        /// <param name="predicate">
+        /// An asynchronous function to evaluate the value contained in the
+        /// <see cref="Option{T}" /> if it is in a "Some" state.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task{TResult}" /> containing <see langword="true" /> if the
+        /// <see cref="Option{T}" /> is in a "None" state, or if it is in a "Some" state
+        /// and the predicate evaluates to <see langword="true" /> for the contained value;
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        [DebuggerStepThrough]
+        public async Task<bool> IsNoneOr(Func<T, Task<bool>> predicate)
+        {
+            Option<T> option = await optionValueTask.ConfigureAwait(false);
 
             return await option.IsNoneOr(predicate).ConfigureAwait(false);
         }
