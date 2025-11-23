1e5836dde1e999667969fe6a0a1f52dc6a6c7973
William Pei <draekien@users.noreply.github.com>
2025-11-23 18:36:56 +1100

    feat: add IsSomeAnd extensions for async Option handling

---
+++ test/Waystone.Monads.Tests/Options/Steps/IsSomeAndExtensionsSteps.cs
@@ -0,0 +1,33 @@
+﻿namespace Waystone.Monads.Options.Steps;
+
+using System;
+using System.Threading.Tasks;
+using Extensions;
+using Reqnroll;
+
+[Binding]
+public class IsSomeAndExtensionsSteps(ScenarioContext context)
+{
+    [When("invoking IsSomeAnd on Option Task with the async predicate")]
+    public async Task WhenInvokingIsSomeAndOnOptionTaskWithTheAsyncPredicate()
+    {
+        var optionTask = context.Get<Task<Option<int>>>();
+        var predicate = context.Get<Func<int, Task<bool>>>();
+
+        bool result = await optionTask.IsSomeAnd(predicate);
+
+        context.Set(result, Constants.ResultKey);
+    }
+
+    [When("invoking IsSomeAnd on Option ValueTask with the async predicate")]
+    public async Task
+        WhenInvokingIsSomeAndOnOptionValueTaskWithTheAsyncPredicate()
+    {
+        var optionTask = context.Get<ValueTask<Option<int>>>();
+        var predicate = context.Get<Func<int, Task<bool>>>();
+
+        bool result = await optionTask.IsSomeAnd(predicate);
+
+        context.Set(result, Constants.ResultKey);
+    }
+}
