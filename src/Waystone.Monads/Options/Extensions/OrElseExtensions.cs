1c6d5ec9d69fd0d42dba57af0d5815bc87b80352
William Pei <draekien@users.noreply.github.com>
2025-11-23 22:57:30 +1100

    feat: add OrElse extensions for async Option handling
    - Introduced `OrElse` methods for `Option<T>`, `Task<Option<T>>`, and `ValueTask<Option<T>>`.
    - Enabled support for both sync and async fallback functions.

---
+++ src/Waystone.Monads/Options/Extensions/OrElseExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Waystone.Monads.Options.Extensions;
+
+using System;
+using System.Threading.Tasks;
+
+public static class OrElseExtensions
+{
+    extension<T>(Option<T> option) where T : notnull
+    {
+        public async ValueTask<Option<T>> OrElse(Func<Task<Option<T>>> elseFunc)
+        {
+            if (option.IsSome) return option;
+
+            return await elseFunc.Invoke().ConfigureAwait(false);
+        }
+    }
+
+    extension<T>(Task<Option<T>> optionTask) where T : notnull
+    {
+        public async Task<Option<T>> OrElse(Func<Option<T>> elseFunc)
+        {
+            Option<T>? option = await optionTask.ConfigureAwait(false);
+
+            return option.IsSome ? option : elseFunc.Invoke();
+        }
+
+        public async Task<Option<T>> OrElse(Func<Task<Option<T>>> elseFunc)
+        {
+            Option<T> option = await optionTask.ConfigureAwait(false);
+
+            if (option.IsSome) return option;
+
+            return await elseFunc.Invoke().ConfigureAwait(false);
+        }
+    }
+
+    extension<T>(ValueTask<Option<T>> optionTask) where T : notnull
+    {
+        public async Task<Option<T>> OrElse(Func<Option<T>> elseFunc)
+        {
+            Option<T>? option = await optionTask.ConfigureAwait(false);
+
+            return option.IsSome ? option : elseFunc.Invoke();
+        }
+
+        public async Task<Option<T>> OrElse(Func<Task<Option<T>>> elseFunc)
+        {
+            Option<T> option = await optionTask.ConfigureAwait(false);
+
+            if (option.IsSome) return option;
+
+            return await elseFunc.Invoke().ConfigureAwait(false);
+        }
+    }
+}
