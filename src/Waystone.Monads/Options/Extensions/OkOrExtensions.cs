acac96d7326962c8cf95096a857a872d725a4800
William Pei <draekien@users.noreply.github.com>
2025-11-23 22:45:04 +1100

    feat: add OkOr extensions for async Option handling
    - Added `OkOr` method support for `Task<Option<T>>` and `ValueTask<Option<T>>`.
    - Enabled default error handling for async `Option` transformations.

---
+++ src/Waystone.Monads/Options/Extensions/OkOrExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Waystone.Monads.Options.Extensions;
+
+using System.Threading.Tasks;
+using Results;
+
+public static class OkOrExtensions
+{
+    extension<T>(Task<Option<T>> optionTask) where T : notnull
+    {
+        public async Task<Result<T, TErr>> OkOr<TErr>(TErr err)
+            where TErr : notnull
+        {
+            Option<T> option = await optionTask.ConfigureAwait(false);
+
+            return option.OkOr(err);
+        }
+    }
+
+    extension<T>(ValueTask<Option<T>> optionTask) where T : notnull
+    {
+        public async Task<Result<T, TErr>> OkOr<TErr>(TErr err)
+            where TErr : notnull
+        {
+            Option<T> option = await optionTask.ConfigureAwait(false);
+
+            return option.OkOr(err);
+        }
+    }
+}
