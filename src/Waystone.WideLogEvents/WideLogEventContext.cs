6835a86f17f42ce6e028d5fbce1d07f98352f680
William Pei <draekien@users.noreply.github.com>
2026-02-07 08:51:02 +1100

    refactor(wide-log-events): remove unused code (#49)

--- src/Waystone.WideLogEvents/WideLogEventContext.cs
+++ src/Waystone.WideLogEvents/WideLogEventContext.cs
@@ -14,8 +14,7 @@ public sealed class WideLogEventContext
     public static WideLogEventScope BeginScope() => new();
 
     public static IReadOnlyDictionary<string, object?> GetScopedProperties() =>
-        ScopedProperties.Value
-     ?? (IReadOnlyDictionary<string, object?>)new Dictionary<string, object?>();
+        ScopedProperties.Value ?? [];
 
     public static void PushProperty(string name, object? value)
     {
