542e796714357ec73493245de485ad3c0b5e8d77
William Pei <draekien@users.noreply.github.com>
2026-09-02 17:18:08 +1000

    test: drop an internal namespace the schema tests never reach

--- test/Waystone.Monads.Schemas.Tests/GlobalUsings.cs
+++ test/Waystone.Monads.Schemas.Tests/GlobalUsings.cs
@@ -1,5 +1,4 @@
 global using Waystone.Monads.Schemas.Internal;
 global using Waystone.Monads.Schemas.Internal.Combinators;
-global using Waystone.Monads.Schemas.Internal.Fields;
 global using Waystone.Monads.Schemas.Internal.Reporting;
 global using Waystone.Monads.Schemas.Internal.Structures;
